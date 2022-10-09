8f349145061c60ddbc9c6c5962621714d459b36b
Colin FitzGerald <colinfitzgerald328@gmail.com>
2022-10-09 13:52:49 -0700

    adding in live workout data

--- pages/workoutPage.js
+++ pages/workoutPage.js
@@ -9,10 +9,34 @@ import styles from "./styles.module.css"
 class WorkoutPage extends react.Component {
     constructor(props) {
         super(props)
+        this.state={data: []}
+    }
+
+    componentDidMount() {
+        this.getWorkoutDetails.bind(this)()
+    }
+
+
+    getWorkoutDetails() {
+
+        var xhr = new XMLHttpRequest();
+        var self = this
+        xhr.onreadystatechange = function() { 
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var data = JSON.parse(xhr.responseText)
+                self.setState({data: data["workout_details"]})
+            }
+        }
+
+
+        const theUrl = "https://weight-workouts.uc.r.appspot.com/getWorkout?timestamp=" + this.props.timestamp + "&account_id=" + localStorage.getItem("accountId")
+
+        xhr.open("GET", theUrl, true); // true for asynchronous 
+        xhr.send(null);
     }
 
     render() {
-        console.log(this.props.data)
+        console.log(this.state.data)
         return(
             <div className={styles.basic}>
                 <Head>
@@ -36,3 +60,12 @@ class WorkoutPage extends react.Component {
 }
 
 export default WorkoutPage
+
+export async function getServerSideProps(context) {
+    
+    var timestamp = context.query.timestamp ?? null
+    var props = {timestamp: timestamp}
+    return {
+        props: props
+    }
+}
