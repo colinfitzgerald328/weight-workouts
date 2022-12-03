fdbc4ab7d737a024c0059d15384d63e34c248736
Colin FitzGerald <colinfitzgerald328@gmail.com>
2022-12-03 00:02:19 -0800

    switching to collapsible cards

--- components/WorkoutBar/index.js
+++ components/WorkoutBar/index.js
@@ -1,41 +1,28 @@
-import {Card, Grid, Text, Button, Row} from "@nextui-org/react";
+import {Collapse, Text, Button} from "@nextui-org/react";
 import {Link, BrowserRouter} from 'react-router-dom'
 import styles from "./styles.module.css"
 import Moment from "react-moment";
 
 export default function WorkoutBar(props) {
     const hrefLink = "/workoutPage?timestamp=" + props.timestamp;
+    const formattedDate = <Moment format={"MM/DD/YYYY"} date={props.timestamp * 1000}/>;
     return (
-        <Grid.Container>
-            <Grid css={{w: "100vw", h: "30vh", margin: "20px"}}>
-                <Card>
-                    <Card.Header>
-                        <Text b>Weight Workout</Text>
-                    </Card.Header>
-                    <Card.Divider/>
-                    <Card.Body css={{py: "$10"}}>
-                        <Text>
-                            <Moment format={"MM/DD/YYYY"} date={props.timestamp * 1000}/>
-                        </Text>
-                        <Text>
-                            Sets: {props.sets}
-                        </Text>
-                        <Text>
-                            Total Reps: {props.total_reps}
-                        </Text>
-                    </Card.Body>
-                    <Card.Divider/>
-                    <Card.Footer>
-                        <Row>
-                            <Link to={hrefLink}>
-                                <Button className={styles.shit} shadow
-                                        color="gradient">View
-                                    Details</Button>
-                            </Link>
-                        </Row>
-                    </Card.Footer>
-                </Card>
-            </Grid>
-        </Grid.Container>
+        <Collapse.Group css={{background: "white", margin: "15px"}} bordered>
+            <Collapse title="Weight Workout" subtitle={formattedDate}>
+                <Text>
+                    Sets: {props.sets}
+                </Text>
+                <Text css={{marginBottom: "10px"}}>
+                    Total Reps: {props.total_reps}
+                </Text>
+                <Link to={hrefLink}>
+                    <Button className={styles.button}
+                            color="gradient">View
+                        Details</Button>
+                </Link>
+            </Collapse>
+        </Collapse.Group>
     );
+
 }
+
