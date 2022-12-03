0d2939994dbe99c3fd0322613478d467c81194bb
Colin FitzGerald <colinfitzgerald328@gmail.com>
2022-12-03 13:40:55 -0800

    restyling main menu

--- components/WorkoutMenuBar/index.js
+++ components/WorkoutMenuBar/index.js
@@ -5,41 +5,20 @@ import 'reactjs-popup/dist/index.css';
 import styles from "./styles.module.css"
 
 class WorkoutMenuBar extends react.Component {
-    constructor(props) {
-        super(props)
-        this.state={"showPopUp": false, numClicks: 0}
-    }
 
-    showPopUp(){
-        if (this.state.numClicks == 0) {
-            this.setState({"showPopUp": true})
-            this.state.numClicks += 1 
-            console.log(this.state.numClicks)
-        } else {
-            this.setState({"showPopUp": false})
-            this.state.numClicks -= 1 
-            console.log(this.state.numClicks)
-        }
-    }
     render() {
-      if (this.state.numClicks == 1) {
-        var popUp = <WorkoutPagePopup data={this.props.data} onLogOut={this.props.onLogOut}/>
-    } else {
-        var popUp = (<div></div>)
-    }
-      return(
-          <div>
-          <div onClick={()=> console.log(this.props)} className={styles.basic}>
-              <div className={styles.text}>
-                  Workout Details
-              </div>
-              <div data={this.props.data} onClick={()=> this.showPopUp()} className={styles.icon}>
-                  <MenuIcon/>
-              </div>
-          </div>
-                          {popUp}
-                          </div>
-          )
+        return (
+            <div>
+                <div onClick={() => console.log(this.props)} className={styles.basic}>
+                    <div className={styles.text}>
+                        Workout Details
+                    </div>
+                    <div className={styles.alignRight}>
+                        <WorkoutPagePopup/>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
--- components/WorkoutPagePopup/index.js
+++ components/WorkoutPagePopup/index.js
@@ -1,46 +1,35 @@
-import react from "react";
+import {Popover, Menu, Position, Button} from "evergreen-ui";
 import Link from "next/link";
-import MenuIcon from '@mui/icons-material/Menu';
-import styles from "./styles.module.css"
 
-class WorkoutPagePopup extends react.Component {
-    constructor(props) {
-        super(props)
-    }
-
-    logOut() {
-        window.localStorage.clear();
-        console.log(localStorage)
-    }
-
-
-    render() {
-        return(
-            <div className={styles.basic}>
-                <Link href={{
-      pathname: '/', query: this.props.data
-    }}>
-                <div className={styles.action}>
-                    Main
-                    </div>
-                </Link>
-                <Link href={{
-      pathname: '/profile', 
-      query: this.props.data}}>
-                <div className={styles.action2}>
-                    Profile
-                </div>
-                </Link>
-                <Link href={{
-      pathname: '/logWorkout', 
-      query: this.props.data}}>
-                <div className={styles.action2}>
-                    Log Workout
-                </div>
-                </Link>
-            </div>
-        )
-    }
+export default function WorkoutPagePopup(props) {
+    return (
+        <Popover
+            position={Position.BOTTOM_LEFT}
+            content={
+                <Menu>
+                    <Menu.Group marginRight={20}>
+                        <Link href={{
+                            pathname: '/',
+                        }}>
+                            <Menu.Item>Main</Menu.Item>
+                        </Link>
+                        <Link href={{
+                            pathname: '/profile',
+                        }}>
+                            <Menu.Item>Profile</Menu.Item>
+                        </Link>
+                        <Link href={{
+                            pathname: '/profile',
+                        }}>
+                            <Menu.Item>
+                                Log Workout
+                            </Menu.Item>
+                        </Link>
+                    </Menu.Group>
+                </Menu>
+            }
+        >
+            <Button marginRight={20}>Menu</Button>
+        </Popover>
+    );
 }
-
-export default WorkoutPagePopup
